5a1f749f8005d1e5f7f72af2bfc632c3dcd5c197
heemin96 <khk8300@naver.com>
2023-02-05 23:43:29 +0900

    중간 저장

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -14,12 +14,12 @@ export default function projects({ projects }) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <h1 className="text-4xl font-bold sm:text-6xl">
+        <h1 className=" font-bold text-5xl mt-10">
           총 프로젝트 :
           <span className="pl-4 text-blue-500">{projects.results.length}</span>
         </h1>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 py-10 gap-8 w-full">
+        <div className=" max-w-4xl grid grid-cols-1 md:grid-cols-2 py-10 gap-8 w-full">
           {projects.results.map((aProject) => (
             <ProjectItem key={aProject.id} data={aProject} />
           ))}
